dcdbb2e0cc455ecc5b4ba3a657fefb22d01a1b59
Jonathan Nesci <jonathan@takram.com>
2022-01-03 00:08:31 +0900

    Updated my akella starting template, then applied it to this code.

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,21 +1,15 @@
-varying vec2 vUv;
+uniform float time;
+uniform float progress;
 uniform sampler2D uTexture;
-uniform sampler2D uDisplacement;
+uniform vec4 resolution;
+varying vec2 vUv;
+varying vec3 vPosition;
 float PI = 3.141592653589793238;
-// uniform float time;
-// uniform float progress;
-// uniform vec4 resolution;
-// varying vec3 vPosition;
 
 void main() {
   // vec4 color = texture2D(uTexture, vUv);
   
-  vec4 displacement = texture2D(uDisplacement, vUv);
-  float theta = displacement.r * 2. * PI;
-  vec2 dir = vec2(sin(theta), cos(theta));
-  vec2 uv = vUv + dir * displacement.r * 0.1;
-  vec4 color = texture2D(uTexture, uv);
-
-  gl_FragColor = color;
-  // gl_FragColor = displacement;  //DEV: for easy debugging
+  // vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
+  gl_FragColor = vec4(vUv, 0., 1.);
+  // gl_FragColor = color;
 }
